b052da30ceff8124f985d05d34074e98d4dd27c8
nikhil1996b <30387741+Nikhil1996B@users.noreply.github.com>
2023-12-26 09:40:37 +0000

    Styling the components

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import classnames from "classnames";
+import classNames from "classnames";
 
 export default function Home() {
   const [movieTitle, setMovieTitle] = useState("");
@@ -31,49 +32,101 @@ export default function Home() {
     dispatch({ type: "LIKE_MOVIE", payload: movie });
 
   const buttonClass = classnames(
-    "ml-4 px-3 py-2 border shadow shadow-xl outline-none rounded-full bg-cyan-700 text-white font-bold hover:opacity-70 uppercase duration-500 hover:scale-110",
+    "flex justify-center items-center ml-4 px-2 py-2 w-sm border shadow text-xs shadow-xl outline-none rounded-full bg-cyan-700 text-white font-bold hover:opacity-70 uppercase duration-500 hover:scale-105",
+  );
+
+  const liClass = classnames(
+    "flex flex-col md:flex-row space-y-3 text-center font-bold md:space-y-0 md: space-x-2 bg-white rounded-lg shadow-xl px-4 py-10",
+  );
+
+  const ulClass = classnames(
+    "mb-6 flex flex-col space-y-6 md:space-y-0 md:space-x-4 md:flex-row",
   );
 
   return (
-    <main className="container mx-auto p-10">
-      <div>
+    <main className="container-full p-10 bg-gray-100 min-h-screen m-0 w-6xl">
+      <div className="mb-2">
         <h1>My movie list</h1>
       </div>
-      <div>
+      <div className="flex flex-col space-y-6 md:flex-row md:space-y-0 md:space-x-4 mb-4">
         <input
           type="text"
+          className="p-2 text-gray-800 outline-none placeholder:text-gray-800 placehoder:uppercase placeholder:font-bold rounded-lg shadow-xl bg-white-100"
           placeholder="Add a movie"
           value={movieTitle}
           onChange={(e) => setMovieTitle(e?.target?.value)}
         />
         <button onClick={handleAddMovie} className={buttonClass}>
-          Add Movie
+          <img src="images/movie.svg" alt="add movie" />{" "}
+          <span className="ml-2">Add Movie</span>
         </button>
       </div>
-      <h2>My Movies</h2>
-      <ul>
+      <h2 className="mb-2">My Movies</h2>
+      <ul className={ulClass}>
         {movies?.map((movie) => (
-          <li key={movie?.title}>
-            {movie?.title}
-            <button
-              onClick={() => handleAddToBasket(movie?.title)}
-              className={buttonClass}
-            >
-              {!movie?.inBasket ? "Add to Basket" : "Remove from Basket"}
-            </button>
-            <button
-              onClick={() => handleAddToLikedMovies(movie?.title)}
-              className={buttonClass}
-            >
-              {!movie?.liked ? "Like" : "Dislike"}
-            </button>
+          <li key={movie?.title} className={liClass}>
+            <h2>{movie?.title}</h2>
+            <div className="flex flex-col lg:flex-row space-y-3 md:space-y-0">
+              <button
+                onClick={() => handleAddToBasket(movie?.title)}
+                className={buttonClass}
+              >
+                {!movie?.inBasket ? (
+                  <>
+                    <img
+                      src="images/cart.svg"
+                      alt="add movie"
+                      className="w-6 h-6"
+                    />{" "}
+                  </>
+                ) : (
+                  <>
+                    <img
+                      src="images/remove-cart.svg"
+                      alt="add movie"
+                      className="w-6 h-6"
+                    />{" "}
+                  </>
+                )}
+              </button>
+              <button
+                onClick={() => handleAddToLikedMovies(movie?.title)}
+                className={buttonClass}
+              >
+                {!movie?.liked ? (
+                  <>
+                    <img src="images/like.svg" alt="Like" className="w-6 h-6" />{" "}
+                  </>
+                ) : (
+                  <>
+                    <img
+                      src="images/dislike.svg"
+                      alt="add movie"
+                      className="w-6 h-6"
+                    />{" "}
+                  </>
+                )}
+              </button>
+            </div>
           </li>
         ))}
       </ul>
-      <h2>My Basket {basket.length}</h2>
-      <ul>{basket?.map((title) => <li key={title}>{title}</li>)}</ul>
-      <h2>Liked Movies {likedMovies.length}</h2>
-      <div>{likedMovies?.map((title) => <li key={title}>{title}</li>)}</div>
+      <h2 className="mb-2">My Basket {basket.length}</h2>
+      <ul className={ulClass}>
+        {basket?.map((title) => (
+          <li className={liClass} key={title}>
+            {title}
+          </li>
+        ))}
+      </ul>
+      <h2 className="mb-2">Liked Movies {likedMovies.length}</h2>
+      <ul className={ulClass}>
+        {likedMovies?.map((title) => (
+          <li className={liClass} key={title}>
+            {title}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
